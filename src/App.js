fdb1c561f7e1cfe793be4d76835286eb3021274b
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-04-01 23:37:52 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './components.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      < Title/>
+      <img src="http://dept.harpercollege.edu/biology/guide/gallery/evidence/scat/original/Bird_Berry_Scat.jpg" className ='flex rounded-lg'alt="poop" ></img>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components.js
@@ -0,0 +1,23 @@
+import "./components.css";
+import { Component } from "react";
+
+class Input extends Component {
+
+    constructor() {
+
+    }
+
+    render() {
+    return <div>Hello</div>;
+    }
+}
+
+class TitleContainer extends Component {
+    render() {
+      return (
+        <div>POOdle</div>
+      );
+    }
+  }
+  
+export default TitleContainer;
