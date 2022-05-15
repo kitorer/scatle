27b1c5161d3e73d825c45497638071849d48213f
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-05-15 14:20:17 -0400

    fixed css for modals

--- src/components/mainBody.js
+++ src/components/mainBody.js
@@ -14,24 +14,28 @@ var tries = [] , recentTry;
 function Input (props) {
     const [submission, setSub] = useState('');
     const [chances, setChances] = useState(2);
-    
+    const [win, setWin] = useState(false);
     
     const handleSubmit = (e) => {
 
-        setChances(chances => chances - 1);
         //guards
         console.log(submission);
-        if(tries.length === 2) {
+        if(tries.length === 2 && submission !== answer) {
             alert("Run out of tries");
+            setWin(false);
+            setChances(chances => chances - 1);
         }
-        if(submission != answer) {
+        else if(submission !== answer) {
             tries.push(submission);
             alert("Wrong! Try again");
+            setChances(chances => chances - 1);
         }
 
         else if(submission === answer) {
             alert("Right answer");
+            setWin(true);
         }
+        console.log(win);
         e.preventDefault();
     }
 
@@ -40,12 +44,18 @@ function Input (props) {
         
         <img src= {todaysPoop} className ='flex rounded-lg mb-10'alt="poop" ></img> {/* replace this with a something that changes the picture everyday*/}
         <form onSubmit={handleSubmit}>
+
         <p>Which animal did this?</p>
             <input 
                 className="placeholder:italic	text-black placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 shadow-sm focus:outline-none focus:border-sky-500" 
                 placeholder="Type your guess" type="text" name="guess"
-                value={submission}
-                onChange={(e) => setSub(e.target.value)}
+                value= {submission}
+                
+                onChange={(e) =>{
+                    setSub(e.target.value)
+                    
+                    } 
+                }
             />
         </form>
 
